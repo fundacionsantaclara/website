d45c387debff7a0d4478fecfaa96eb18b3b5f5b5
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-06-11 21:35:34 +0200

    fundacion imgs

--- src/components/Pages/EventosData.js
+++ src/components/Pages/EventosData.js
@@ -1,53 +1,52 @@
-import React from 'react'
-import EventosCard from './Eventos';
-import './Eventos.css'
-import Sagrada from '../Fcimages/sagrada familia.jpg'
-import Recapte from '../Fcimages/gran recapte.jpg'
-import Vinya from '../Fcimages/vinya cross2.png'
-
-
+import React from "react";
+import EventosCard from "./Eventos";
+import "./Eventos.css";
+import Sagrada from "../Fcimages/sagrada familia.jpg";
+import Recapte from "../Fcimages/gran recapte.jpg";
+import Vinya from "../Fcimages/vinya cross2.png";
+import ukranians from "../../Images/ucrania.jpg";
 
 const events = [
   {
     id: 1,
-    imageSrc: Vinya,
-    eventName: 'Vinya cross',
-    date: 'Noviembre 19, 2023',
-    time: '2:00 PM',
+    imageSrc: ukranians,
+    eventName: "250 sonrisas",
+    date: "Jun, 2024",
+    time: "2:00 PM",
   },
   {
     id: 1,
     imageSrc: Recapte,
-    eventName: 'Gran recapte',
-    date: 'Noviembre 24, 2023, Noviembre 25, 2023',
-    time: '2:00 PM',
+    eventName: "Gran recapte",
+    date: "May, 2024",
+    time: "2:00 PM",
   },
   {
     id: 1,
     imageSrc: Sagrada,
-    eventName: 'Missa Criolla',
-    date: 'Diciembre 12, 2023',
-    time: '2:00 PM',
+    eventName: "Missa Criolla",
+    date: "Dec 12, 2023",
+    time: "2:00 PM",
   },
   // ... other events
 ];
-const EventosData = ()=> {
+const EventosData = () => {
   return (
     <di>
-         <h1 className="event-title">Próximos Eventos</h1>
-    <div className="Event-container">
-       {events.map(event => (
-        <EventosCard
-          key={event.id}
-          imageSrc={event.imageSrc}
-          eventName={event.eventName}
-          date={event.date}
-          time={event.time}
-        />
-      ))}
-    </div>
+      <h1 className="event-title">Próximos Eventos</h1>
+      <div className="Event-container">
+        {events.map((event) => (
+          <EventosCard
+            key={event.id}
+            imageSrc={event.imageSrc}
+            eventName={event.eventName}
+            date={event.date}
+            time={event.time}
+          />
+        ))}
+      </div>
     </di>
   );
-}
+};
 
-export default EventosData
+export default EventosData;
