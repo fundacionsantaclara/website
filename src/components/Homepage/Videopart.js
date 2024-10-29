e041f0aba7831667b5eba70242dfdc603b9608ac
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-10-29 10:31:34 +0100

    add the imageinfo to reeplace the videoloop in home, the next time we need change and compllte with really info in canva

--- src/components/Homepage/Videopart.js
+++ src/components/Homepage/Videopart.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Slider from "react-slick";
+import { useTranslation } from "react-i18next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import esImage from "../../Images/infoCastellano.png"
+import caImage from "../../Images/infoCatalan.png"
+import enImage from "../../Images/infoEnglish.png"
 import misa from "../../Images/EVENTO SAGRADA FAMILIA 2023-104.jpg";
 import misa2 from "../../Images/Libro2.jpg";
 import misa3 from "../../Images/ucrania.jpg";
@@ -10,6 +14,8 @@ import misa4 from "../../Images/ucrania6.jpg";
 import "./Videopart.css";
 
 const Videopart = () => {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
   const settings = {
     dots: true,
     infinite: true,
@@ -20,29 +26,32 @@ const Videopart = () => {
     autoplaySpeed: 2000, // Adjust the speed in milliseconds
   };
 
+   // Selección de imagen estática según el idioma
+   const staticImage =
+   currentLanguage === "es"
+     ? esImage
+     : currentLanguage === "ca"
+     ? caImage
+     : enImage;
+
+ // Imágenes del carrusel
+ const carouselImages = [misa, misa2, misa3, misa4];
+
   return (
     <div className="video-page-container">
-      <div className="video-section">
-        {/* You can replace the video source with your own */}
-        <video width="100%" height="100%" controls loop autoPlay muted>
-          <source src="/fundacionconvent.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    {/* Contenedor de la imagen que cambia según el idioma */}
+    <div className="video-section">
+      <img src={staticImage} alt="Idioma seleccionado" width="100%" height="100%" />
+    </div>
+
+      {/* Contenedor del carrusel */}
       <div className="slideshow-section">
         <Slider {...settings}>
-          <div className="slide">
-            <img src={misa} alt="Slide 1" />
-          </div>
-          <div className="slide">
-            <img src={misa2} alt="Slide 2" />
-          </div>
-          <div className="slide">
-            <img src={misa3} alt="Slide 2" />
-          </div>
-          <div className="slide">
-            <img src={misa4} alt="Slide 2" />
-          </div>
+          {carouselImages.map((imgSrc, index) => (
+            <div className="slide" key={index}>
+              <img src={imgSrc} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
         </Slider>
       </div>
     </div>
