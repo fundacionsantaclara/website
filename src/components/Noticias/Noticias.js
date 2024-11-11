803af4bdd3973ed1565aac2f7ca434c0ed604a3c
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-11-11 13:28:25 +0100

    add the system to order the news and add the new section where the people would see  the all news in the historial when the user make clik in the show more button

--- src/components/Noticias/Noticias.js
+++ src/components/Noticias/Noticias.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import "./Noticias.css";
 import { Link } from "react-router-dom";
@@ -7,22 +7,29 @@ const Noticias = () => {
   const { t } = useTranslation();
   const noticias = t("news", { returnObjects: true });
 
-  return (
-    <div className="noticias-card-container">
-      <h1 className="noticia-title">{t("latest_news")}</h1>
-      <div className="noticias-card-list">
-        {noticias.map((noticia) => (
-          <div className="noticias-card" key={noticia.id}>
-            <Link to={`/News/${noticia.id}`}>
-              <img src={noticia.carrousel1} alt={noticia.title} />
-              <h2>{noticia.title}</h2>
-              <p>{noticia.subtitle}</p>
-            </Link>
-          </div>
-        ))}
+  // Ordena las noticias en orden descendente de ID y toma las tres primeras
+  const ultimasNoticias = noticias
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 3);
+
+    return (
+      <div className="noticias-card-container">
+        <h1 className="noticia-title">{t("latest_news")}</h1>
+        <div className="noticias-card-list">
+          {ultimasNoticias.map((noticia) => (
+            <div className="noticias-card" key={noticia.id}>
+              <Link to={`/News/${noticia.id}`}>
+                <img src={noticia.carrousel1} alt={noticia.title} />
+                <h2>{noticia.title}</h2>
+                <p>{noticia.subtitle}</p>
+                <p className="noticia-ubicacion">{noticia.ubicacion}</p> {/* Agrega esta línea */}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Link to="/NoticiasCompleta" className="ver-mas-boton">Ver más</Link>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Noticias;
---
+++ src/components/Noticias/NoticiasCompleta.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./Noticias.css";
+import { Link } from "react-router-dom";
+
+const NoticiasCompleta = () => {
+  const { t } = useTranslation();
+  const noticias = t("news", { returnObjects: true });
+
+  const noticiasOrdenadas = noticias.sort((a, b) => b.id - a.id);
+
+  return (
+    <div className="noticias-card-container">
+      <h1 className="noticia-title">{t("latest_news")}</h1>
+      <div className="noticias-card-list completa-list">
+        {noticias.map((noticia) => (
+          <div className="noticias-card" key={noticia.id}>
+            <Link to={`/News/${noticia.id}`}>
+              <img src={noticia.carrousel1} alt={noticia.title} />
+              <h2>{noticia.title}</h2>
+              <p>{noticia.subtitle}</p>
+              <p className="noticia-ubicacion">{noticia.ubicacion}</p> {/* Agrega esta línea */}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NoticiasCompleta;
