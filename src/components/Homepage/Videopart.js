41d61c1e5f5f6643df20ffd54009c47c3fbd839e
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-11-15 16:57:01 +0100

    improve the infosection in home, now only need that Peter say me what´s information insert here

--- src/components/Homepage/Videopart.js
+++ src/components/Homepage/Videopart.js
@@ -3,19 +3,14 @@ import Slider from "react-slick";
 import { useTranslation } from "react-i18next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import esImage from "../../Images/infoCastellano.png"
-import caImage from "../../Images/infoCatalan.png"
-import enImage from "../../Images/infoEnglish.png"
 import misa from "../../Images/EVENTO SAGRADA FAMILIA 2023-104.jpg";
 import misa2 from "../../Images/Libro2.jpg";
 import misa3 from "../../Images/ucrania.jpg";
 import misa4 from "../../Images/ucrania6.jpg";
-
 import "./Videopart.css";
 
 const Videopart = () => {
-  const { i18n } = useTranslation();
-  const currentLanguage = i18n.language;
+  const { t } = useTranslation();
   const settings = {
     dots: true,
     infinite: true,
@@ -26,23 +21,25 @@ const Videopart = () => {
     autoplaySpeed: 2000, // Adjust the speed in milliseconds
   };
 
-   // Selección de imagen estática según el idioma
-   const staticImage =
-   currentLanguage === "es"
-     ? esImage
-     : currentLanguage === "ca"
-     ? caImage
-     : enImage;
-
- // Imágenes del carrusel
- const carouselImages = [misa, misa2, misa3, misa4];
+  // Imágenes del carrusel
+  const carouselImages = [misa, misa2, misa3, misa4];
 
   return (
     <div className="video-page-container">
-    {/* Contenedor de la imagen que cambia según el idioma */}
-    <div className="video-section">
-      <img src={staticImage} alt="Idioma seleccionado" width="100%" height="100%" />
-    </div>
+      {/* Contenedor de la imagen de fondo con el texto */}
+      <div className="text-background-section">
+        <div className="text-section">
+          <h2 className="help-title">{t('videoPage.title')}</h2>
+          <ul className="help-list">
+            <li>{t('videoPage.infancias')}</li>
+            <li>{t('videoPage.alimentos')}</li>
+            <li>{t('videoPage.pisosDeAcogida')}</li>
+            <li>{t('videoPage.refugiadosYHeridos')}</li>
+            <li>{t('videoPage.regularizacionDePapeles')}</li>
+          
+          </ul>
+        </div>
+      </div>
 
       {/* Contenedor del carrusel */}
       <div className="slideshow-section">
@@ -57,5 +54,4 @@ const Videopart = () => {
     </div>
   );
 };
-
 export default Videopart;
