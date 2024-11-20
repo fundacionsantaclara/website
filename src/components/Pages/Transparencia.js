615347710bca3c322acf327e1a7d2792b464589c
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-11-20 13:09:27 +0100

    add files and new parts in the body, only need complete translations in Ca and En

--- src/components/Pages/Transparencia.js
+++ src/components/Pages/Transparencia.js
@@ -19,11 +19,10 @@ import cuentas2019 from "../Documents/Comptes_anuals_2019.pdf";
 import auditoría2020 from "../Documents/INFORME_AUDITORIA_I_CCAA_2020.pdf";
 import auditoría2021 from "../Documents/INFORME_AUDITORIA_I_CCAA_2021.pdf";
 import auditoría2022 from "../Documents/INFO_AUDITORIA_CCAA_2022_FCSC.pdf";
-
-
-
-
-
+import audiotria2023 from "../Documents/INFO_AUDITORIA_CCAA_2023_FCSC.pdf"
+import memo2023Novisual from "../Documents/memoria_FRO_2023.pdf"
+import convenioDeColaboracionVigente from "../Documents/Convenis-de-col.laboracio-vigents.pdf"
+import subencionesAdministracion from "../Documents/AJUTS_I_SUBVENCIONS_ADMINISTRACIONS_PUBLIQUES.pdf"
 
 
 
@@ -40,133 +39,180 @@ function Transparencia() {
               </h1>
             </div>
             <p className="para__text text__grey">{t("transparency-intro")}</p>
+
+            {/* Primer Tarjeta */}
             <div className="trans-card">
-              <img src="https://www.sharda.ac.in/blog/attachments/blog_images/4-Main-Types-of-Law-Which-One-Is-the-Best-For-You-1170x614-min.jpg" />
-              <div className="trans-card-content">
-                <h2>{t("transparency-card1-title")}</h2>
-                <h3>{t("transparency-card1-subtitle")}</h3>
-                <h5>{t("transparency-card1-subtitle2")}</h5>
-                <ul className="trans-list">
+              <img src="https://www.sharda.ac.in/blog/attachments/blog_images/4-Main-Types-of-Law-Which-One-Is-the-Best-For-You-1170x614-min.jpg" alt="Law Image"/>
+              <div className="trans-card-content-wrapper">
+                <div className="trans-card-content">
+                  <h2>{t("transparency-card1-title")}</h2>
+                  <h3>{t("transparency-card1-subtitle")}</h3>
+                  <h5>{t("transparency-card1-subtitle2")}</h5>
+                  <ul className="trans-list">
+                    <p>
+                      <a className="link-offset-1" href={patrons} target="_blank">
+                        {t("patrons-title")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-1" href={estatutos} target="_blank">
+                        {t("estatutos-title")}
+                      </a>
+                    </p>
+                    <p> 
+                      <a className="link-offset-1" href={registros} target="_blank">
+                        {t("registros-title")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-1" href={normativos} target="_blank">
+                        {t("normativo-title")}
+                      </a>
+                    </p>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* Segunda Tarjeta */}
+            <div className="trans-card">
+              <img src="https://uploads-ssl.webflow.com/575ef60509a5a7a9116d9f8c/5ea21e418558777085ce093a_memoria%20contable%20ERP%20nube%20administracion%20empresas.jpg" alt="Accounting Image"/>
+              <div className="trans-card-content-wrapper">
+                <div className="trans-card-content">
+                  <h2>{t("organigrama-title")}</h2>
+                  <ul className="trans-list">
                   <p>
-                    <a class="link-offset-1" href={patrons}>
-                      {t("patrons-title")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-1" href={estatutos}>
-                      {t("estatutos-title")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-1" href={registros}>
-                      {t("registros-title")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-1" href={normativos}>
-                      {t("normativo-title")}
-                    </a>
-                  </p>
-                </ul>
+                      <a className="link-offset-2" href={plan2016} target="_blank">
+                        {t("plan2016-2017")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-1" href={memo2018} target="_blank">
+                        {t("memoria-2018")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-2" href={memo2019} target="_blank">
+                        {t("memoria-2019")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={memo2020} target="_blank">
+                        {t("memoria-2020")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={memo2021} target="_blank">
+                        {t("memoria-2021")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={memo2022} target="_blank">
+                        {t("memoria-2022")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={memo2023Novisual} target="_blank">
+                        {t("memoria-2023-no-visual")}
+                      </a>
+                    </p>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* Tercera Tarjeta */}
+            <div className="trans-card">
+              <img src="https://bradfordjacobs.com/wp-content/uploads/2021/07/economy.jpg" alt="Economy Image"/>
+              <div className="trans-card-content-wrapper">
+                <div className="trans-card-content">
+                  <h2>{t("info-economica-title")}</h2>
+                  <ul className="trans-list">
+                    <p>
+                      <a className="link-offset-1" href={cuentas2015} target="_blank">
+                        {t("cuentas-2015")}
+                      </a>
+                    </p>
+                    
+                    <p>
+                      <a className="link-offset-3" href={cuentas2016} target="_blank">
+                        {t("cuentas-2016")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={cuentas2017} target="_blank">
+                        {t("cuentas-2017")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={cuentas2018} target="_blank">
+                        {t("cuentas-2018")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={cuentas2019} target="_blank">
+                        {t("cuentas-2019")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={auditoría2020} target="_blank">
+                        {t("Informe Auditoría 2020")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={auditoría2021} target="_blank">
+                        {t("Informe Auditoría 2021")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-3" href={auditoría2022} target="_blank">
+                        {t("Informe Auditoría 2022")}
+                      </a>
+                    </p>
+                    <p>
+                      <a className="link-offset-1" href={audiotria2023} target="_blank" >
+                        {t("Informe Auditoría 2023")}
+                      </a>
+                    </p>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+          {/* Cuarta Tarjeta */}
+          <div className="trans-card">
+              <img src="https://bradfordjacobs.com/wp-content/uploads/2021/07/economy.jpg" alt="Economy Image"/>
+              <div className="trans-card-content-wrapper">
+                <div className="trans-card-content">
+                  <h2>{t("info-gestionAdministrativa-title")}</h2>
+                  <ul className="trans-list">
+                    <p>
+                      <a className="link-offset-1" href={convenioDeColaboracionVigente} target="_blank">
+                        {t("convenio-colaboracion-vigente")}
+                      </a>
+                    </p>
+                    
+                    <p>
+                      <a className="link-offset-3" href={subencionesAdministracion} target="_blank">
+                        {t("subenciones-administrativas")}
+                      </a>
+                    </p>
+                  </ul>
+                </div>
               </div>
             </div>
 
             <div className="trans-card">
-              <img src="https://uploads-ssl.webflow.com/575ef60509a5a7a9116d9f8c/5ea21e418558777085ce093a_memoria%20contable%20ERP%20nube%20administracion%20empresas.jpg" />
-              <div className="trans-card-content">
-                <h2>{t("organigrama-title")}</h2>
-                <ul className="trans-list">
-                  <p>
-                    <a class="link-offset-1" href={memo2018}>
-                      {t("memoria-2018")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-2" href={memo2019}>
-                      {t("memoria-2019")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={memo2020}>
-                      {t("memoria-2020")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={memo2021}>
-                    {t("memoria-2021")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={memo2022}>
-                    {t("memoria-2022")}
-                    </a>
-                  </p>
-                </ul>
-              </div>
-            </div>
+  <img src="https://www.yourdomain.com/path-to-image.jpg" alt="Image Description" />
+  <div className="trans-card-content-wrapper">
+    <div className="trans-card-content">
+      <h2>{t("retribucion-title")}</h2>
+      <p dangerouslySetInnerHTML={{ __html: t("retribucion-description1") }}></p>
+      <p dangerouslySetInnerHTML={{ __html: t("retribucion-description2") }}></p>
+    </div>
+  </div>
+</div>
 
-            <div className="trans-card">
-              <img src="https://bradfordjacobs.com/wp-content/uploads/2021/07/economy.jpg" />
-              <div className="trans-card-content">
-                <h2>{t("info-economica-title")}</h2>
-                <ul className="trans-list">
-                  <p>
-                    <a class="link-offset-1" href={cuentas2015}>
-                      {t("cuentas-2015")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-2" href={plan2016}>
-                     {t("plan2016-2017")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={cuentas2016}>
-                    {t("cuentas-2016")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={cuentas2017}>
-                      {t("cuentas-2017")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={cuentas2018}>
-                    {t("cuentas-2018")}
-                    </a>
-                  </p>
-                </ul>
-              </div>
-            </div>
-
-            <div className="trans-card">
-              <img src="https://bradfordjacobs.com/wp-content/uploads/2021/07/economy.jpg" />
-              <div className="trans-card-content">
-                <ul className="trans-list">
-                  <p>
-                    <a class="link-offset-3" href={cuentas2019}>
-                    {t("cuentas-2019")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={auditoría2020}>
-                  {t("Informe Auditoría 2020")}
-
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={auditoría2021}>
-                    {t("Informe Auditoría 2021")}
-                    </a>
-                  </p>
-                  <p>
-                    <a class="link-offset-3" href={auditoría2022}>
-                    {t("Informe Auditoría 2022")}
-                    </a>
-                  </p>
-                </ul>
-              </div>
-            </div>
           </div>
         </div>
       </div>
