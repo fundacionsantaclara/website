4ea33f2bec7bfeba7dc8492fa6116aacb92fafa6
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-11-26 15:45:18 +0100

    improvement the  taller css section

--- src/components/Pages/Taller.js
+++ src/components/Pages/Taller.js
@@ -9,39 +9,35 @@ import TallerEmail from "./Talleremail";
 
 function Taller() {
   const { t } = useTranslation();
+
   return (
-    <div>
-      <div className="taller section__padding">
-        <div className="taller-container">
-          <div className="taller__content text__center">
-            <div className="taller-header">
-              <h1 className="tallersection__title text__cap">
-                {t("taller-page-title")}
-              </h1>
-            </div>
-            <div className="taller-part1">
-              <div className="taller__text">
-                <h3 className="taller-text-title">{t("taller-card1-title")}</h3>
-                <div className="paragraph-text">
-                  <p className=" text__grey">{t("taller-card1-para")}</p>
-                  <p>{t("taller-card1-para2")}</p>
-                </div>
-              </div>
-              <TallerCarousel />
-              <div className="para-text2">
-                <p>{t("taller-card2-para")}</p>
-                <p>{t("taller-card2-para2")}</p>
-                <p>{t("taller-card2-para3")}</p>
-                <div className="btn-taller">
-                  <TallerWhatsappBtn />
-                  <TallerEmail />
-                </div>
-              </div>
-              <TallerCarouselTheme />
-              <TallerStorybook />
-              <div></div>
+    <div className="taller">
+      <div className="taller-header">
+        <h1 className="tallersection__title text__cap">
+          {t("taller-page-title")}
+        </h1>
+      </div>
+      <div className="taller-container">
+        <div className="taller__content text__center">
+          <div className="taller__text">
+            <h3 className="taller-text-title">{t("taller-card1-title")}</h3>
+            <div className="paragraph-text">
+              <p className="text__grey">{t("taller-card1-para")}</p>
+              <p>{t("taller-card1-para2")}</p>
             </div>
           </div>
+          <TallerCarousel />
+          <div className="para-text2">
+            <p>{t("taller-card2-para")}</p>
+            <p>{t("taller-card2-para2")}</p>
+            <p>{t("taller-card2-para3")}</p>
+            <div className="btn-taller">
+              <TallerWhatsappBtn />
+              <TallerEmail />
+            </div>
+          </div>
+          <TallerCarouselTheme />
+          <TallerStorybook />
         </div>
       </div>
     </div>
