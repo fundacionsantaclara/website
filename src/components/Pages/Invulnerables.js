1ede2fc6c430e58d25e44c057f416f12e02c6782
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-11-27 09:00:05 +0100

    improvement invulnerable section with css

--- src/components/Pages/Invulnerables.js
+++ src/components/Pages/Invulnerables.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Invulnerables.css";
-import Invulnerable from "../Fcimages/Invulnerables.png";
 import InvulnerablePic from "../Fcimages/Juny.jpg";
 import InvulnerableColl from "../Fcimages/invul-img.jpg";
 import InvulnerableColl2 from "../Fcimages/invul-img2.jpg";
@@ -13,18 +12,22 @@ function Invulnerables() {
   return (
     <div>
       <div className="invulnerables section__padding">
+      <div className="invulnerables-header">
+              <h1 className="invulnerables-section__title text__cap">
+                {t("invulnerables-page-title")}
+              </h1>
+            </div>
+        
         <div className="invulnerables-container">
           <div className="invulnerables__content text__center">
-            <div className="invulnerables-header">
-              <img src={Invulnerable} class="invulnerables-img" alt="..." />
-            </div>
+            
             <p className="invulnerables__text text__grey">
               {t("inv-description")}
             </p>
           </div>
           <div className="invulnerables-card2-container">
             <div className="invul-card">
-              <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
+              <img src={InvulnerablePic} className="invulnerables-img" alt="..." />
               <h2>{t("program-title")}</h2>
               <p>
                 <strong>{t("strong-inv")}</strong> {t("p-inv")}
@@ -33,7 +36,7 @@ function Invulnerables() {
             <div className="invul-card">
               <h2>{t("igual")}</h2>
               <p>{t("p-igual")}</p>
-              <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
+              <img src={InvulnerablePic} className="invulnerables-img" alt="..." />
             </div>
             <div className="invul-card">
               <h2>{t("recur")}</h2>
@@ -42,25 +45,13 @@ function Invulnerables() {
                 <strong>{t("p-recur-strong")}</strong>
               </p>
               <p>{t("p-recur-2")}</p>
-              <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
+              <img src={InvulnerablePic} className="invulnerables-img" alt="..." />
             </div>
             <div className="invul-image-collage">
               <img src={InvulnerableColl} alt="..." className="collage-image" />
-              <img
-                src={InvulnerableColl2}
-                className="collage-image"
-                alt="..."
-              />
-              <img
-                src={InvulnerableColl3}
-                className="collage-image"
-                alt="..."
-              />
-              <img
-                src={InvulnerableColl4}
-                className="collage-image"
-                alt="..."
-              />
+              <img src={InvulnerableColl2} className="collage-image" alt="..." />
+              <img src={InvulnerableColl3} className="collage-image" alt="..." />
+              <img src={InvulnerableColl4} className="collage-image" alt="..." />
             </div>
           </div>
         </div>
