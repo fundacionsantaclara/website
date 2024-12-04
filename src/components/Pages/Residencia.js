b83a3f54678f43b3bc347785830b2cf11b293015
Tulio Toledo Bigon <tuliontoledobigon2@gmail.com>
2024-12-04 13:22:14 +0100

    add the text in the recidense section and better de css

--- src/components/Pages/Residencia.js
+++ src/components/Pages/Residencia.js
@@ -4,74 +4,83 @@ import resifoto from '../Fcimages/fotos de residencia.jpg';
 import resifototwo from '../Fcimages/resi foto2.jpg';
 import { useTranslation } from "react-i18next";
 
-
-
-
-
 function Residencia() {
   const { t } = useTranslation();
   return (
     <div>
-      <div className='resi-header'><h1 className='section__title text__cap'>{t("residencia-page-title")}</h1></div>
-    <div className='resi section__padding'>
-      <div className='resi-container'>
-          <div className='resi__content text__center'>
-              
-              <div class="resi-card">
-    <p class="Resi-card-text">{t("residencia-page-intro1")}</p>
-    <p class="Resi-card-text"><small class="text-body-secondary">{t("residencia-page-intro2")}</small></p>
-  </div>
-  <div className='resi-foto'>
-  <img src={resifoto} class="card-img-bottom" alt="..."/>
-  <img src={resifototwo} class="card-img-bottom" alt="..."/>
-  </div>
-  <div className='resi-small-container'>
-  <div className='smallpart-section'>
-  <div className='resi-card-small'>
-  <div class="card-body text-success">
-    <h4 class="Resi-card-title"><strong>{t("residencia-page-card1-title")}</strong></h4>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card1-info1-bold")}</strong> {t("residencia-page-card1-info1")}</p>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card1-info2-bold")}</strong> {t("residencia-page-card1-info2")} </p>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card1-info3-bold")}</strong> {t("residencia-page-card1-info3")}</p>
-
-  </div>
-  </div>
-  </div>
-  <div className='smallpart-section'>
-  <div className='resi-card-small'>
-  <div class="card-body text-success">
-    <h4 class="Resi-card-title"><strong>{t("residencia-page-card2-title")}</strong></h4>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card2-info1-bold")}</strong>  {t("residencia-page-card2-info1")}</p>
-    <h4 class="Resi-card-title"><strong>{t("residencia-page-card2-title2")}</strong></h4>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card2-info2-bold")}</strong> {t("residencia-page-card2-info2")}</p>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card2-info3-bold")}</strong> {t("residencia-page-card2-info3")}</p>
-    <p class="Resi-card-text"><strong>{t("residencia-page-card2-info4-bold")}</strong> {t("residencia-page-card2-info4")}</p>
-
-
-
-  </div>
-  </div>
-  </div>
-  <div className='smallpart-section'>
-  <div className='resi-card-small'>
-  <div class="card-body text-success">
-    <h4 class="Resi-card-title"><strong>{t("residencia-page-card3-title")}</strong></h4>
-    <p class="Resi-card-text">{t("residencia-page-card3-info")}</p>
-  
-  </div>
-  </div>
-  </div>
-  </div>
-  <div class="resi-para-card-body">
-<p>{t("residencia-page-card4-info")}</p>
-<p><strong>{t("residencia-page-card4-info-bold")}</strong></p>
-  </div>
-  
+      {/* Header Section */}
+      <div className='resi-header'>
+        <h1 className='section__title text__cap'>{t("residencia-page-title")}</h1>
       </div>
+      
+      {/* Main Content Section */}
+      <div className='resi section__padding'>
+        <div className='resi-container'>
+          <div className='resi__content'>
+            {/* Introduction Section */}
+            <div className='resi-intro'>
+              <p className='resi-card-text'>{t("residencia-page-intro1")}</p>
+              <p className='resi-card-text'><small className='text-body-secondary'>{t("residencia-page-intro2")}</small></p>
+            </div>
+
+            {/* Image Section */}
+            <div className='resi-foto'>
+              <img src={resifoto} className='card-img-bottom' alt='Foto de la residencia' />
+              <img src={resifototwo} className='card-img-bottom' alt='Otra vista de la residencia' />
+            </div>
+
+            {/* Objectives Section */}
+            <div className='resi-objectives'>
+              <div className='resi-objective-card'>
+                <h4 className='resi-card-title'><strong>{t("residencia-page-objectives-title")}</strong></h4>
+                <p className='resi-card-text'>{t("residencia-page-objectives-desc")}</p>
+              </div>
+            </div>
+
+            {/* Services and Team Section */}
+            <div className='resi-small-container'>
+              {/* Services Card */}
+              <div className='resi-card-small'>
+                <div className='card-body'>
+                  <h4 className='resi-card-title'><strong>{t("residencia-page-card1-title")}</strong></h4>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card1-info1-bold")}</strong> {t("residencia-page-card1-info1")}</p>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card1-info2-bold")}</strong> {t("residencia-page-card1-info2")}</p>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card1-info3-bold")}</strong> {t("residencia-page-card1-info3")}</p>
+                </div>
+              </div>
+
+              {/* Team Card */}
+              <div className='resi-card-small'>
+                <div className='card-body'>
+                  <h4 className='resi-card-title'><strong>{t("residencia-page-card2-title")}</strong></h4>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card2-info1-bold")}</strong> {t("residencia-page-card2-info1")}</p>
+                  <h4 className='resi-card-title'><strong>{t("residencia-page-card3-title")}</strong></h4>
+                  <p className='resi-card-text'>{t("residencia-page-card3-info")}</p>
+                </div>
+              </div>
+
+              {/* Protocol Card */}
+              <div className='resi-card-small'>
+                <div className='card-body'>
+                 
+                  <h4 className='resi-card-title'><strong>{t("residencia-page-card2-title2")}</strong></h4>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card2-info2-bold")}</strong> {t("residencia-page-card2-info2")}</p>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card2-info3-bold")}</strong> {t("residencia-page-card2-info3")}</p>
+                  <p className='resi-card-text'><strong>{t("residencia-page-card2-info4-bold")}</strong> {t("residencia-page-card2-info4")}</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Additional Information Section */}
+            <div className='resi-additional-info'>
+              <p>{t("residencia-page-card4-info")}</p>
+              <p><strong>{t("residencia-page-card4-info-bold")}</strong></p>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>        
-  </div>
+    </div>
   )
 }
 
-export default Residencia
+export default Residencia;
