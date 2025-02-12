1058dc3d8ab5415d54ecf63ec7562228a771e9fe
TulioToledoB <tuliontoledobigon2@gmail.com>
2025-02-12 08:57:51 +0100

    new event

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,55 +17,58 @@ function Footer() {
           <h2 className="support-text">{t("footer-p2")}</h2>
         </div>
         <div class="footer-images">
-            <div class="footer-link-items">
-              <img src={generalitat} alt="Generalitat" />
-              <img src={fundacioCaixa} alt="FundacioCaixa" />
-              <img src={manresa} alt="Manresa" />
+          <div class="footer-link-items">
+            <img src={generalitat} alt="Generalitat" />
+            <img src={fundacioCaixa} alt="FundacioCaixa" />
+            <img src={manresa} alt="Manresa" />
           </div>
         </div>
       </section>
       <section class="social-media">
-  <div class="social-media-wrap">
-    <div class="footer-logo">
-      <Link to="/" className="social-logo">
-        <img className="fsclogo" src={logo} alt="Fundación Santa Clara Logo" />
-      </Link>
-    </div>
-    <small class="website-rights">
-      Fundació convent de Santa Clara © 2024
-    </small>
-    <div class="social-icons">
-      <a
-        class="social-icon-link facebook"
-        href="https://www.facebook.com/fundaciorosaoriol/" 
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Facebook"
-      >
-        <i class="fab fa-facebook-f" />
-      </a>
-      <a
-        class="social-icon-link instagram"
-        href="https://www.instagram.com/fundsantaclara/" 
-        rel="noopener noreferrer"
-        aria-label="Instagram"
-      >
-        <i class="fab fa-instagram" />
-      </a>
-      
-      <a
-        class="social-icon-link twitter"
-        href="https://x.com/i/flow/login?redirect_after_login=%2Ffundsantaclara"  
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Twitter"
-      >
-        <i class="fab fa-twitter" />
-      </a>
-      
-    </div>
-  </div>
-</section>
+        <div class="social-media-wrap">
+          <div class="footer-logo">
+            <Link to="/" className="social-logo">
+              <img
+                className="fsclogo"
+                src={logo}
+                alt="Fundación Santa Clara Logo"
+              />
+            </Link>
+          </div>
+          <small class="website-rights">
+            Fundació convent de Santa Clara © 2024
+          </small>
+          <div class="social-icons">
+            <a
+              class="social-icon-link facebook"
+              href="https://www.facebook.com/fundaciorosaoriol/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+            >
+              <i class="fab fa-facebook-f" />
+            </a>
+            <a
+              class="social-icon-link instagram"
+              href="https://www.instagram.com/fundsantaclara/"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <i class="fab fa-instagram" />
+            </a>
+
+            <a
+              class="social-icon-link twitter"
+              href="https://x.com/i/flow/login?redirect_after_login=%2Ffundsantaclara"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
+              <i class="fab fa-twitter" />
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
