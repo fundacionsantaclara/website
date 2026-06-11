6c988c80b8e1e7b8000df9440ccc7a0aa6c27372
TulioToledoB <tuliontoledobigon2@gmail.com>
2026-06-11 16:11:21 +0200

    Replace local videos with YouTube embeds

--- src/components/Homepage/MovieSection.js
+++ src/components/Homepage/MovieSection.js
@@ -1,18 +1,38 @@
 import React from "react";
 import "./MovieSection.css";
-import video1 from "../Fcimages/video/caravana.mov";
 import { useTranslation } from "react-i18next";
 
 const MovieSection = () => {
   const { t } = useTranslation();
+
   return (
     <div className="video-page-full">
       <h1 className="video-title">{t("corredores")}</h1>
 
-      <div className="video-wrapper">
-        <video controls>
-          <source src={video1} type="video/mp4" />
-        </video>
+      <div className="videos-container">
+        <div className="video-card">
+          <div className="video-wrapper">
+            <iframe
+              src="https://www.youtube.com/embed/GOSowwpVx_8"
+              title="Video 1"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
+
+        <div className="video-card">
+          <div className="video-wrapper">
+            <iframe
+              src="https://www.youtube.com/embed/K6L3GU3kJrM"
+              title="Video 2"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
